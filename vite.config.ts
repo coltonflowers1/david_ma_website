9ee3dc223ffa085e156db129c2874428827f6b0b
Colton Flowers <cflowers.flowers@gmail.com>
2025-08-28 01:31:55 -0400

    Update Vite configuration to set base URL to root for deployment. This change simplifies asset path management across the project.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/david_ma_website/',
+    base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
